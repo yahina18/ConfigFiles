9916ea0239c29a8c7df84bf0cda26283ecdeb56f
Hina Yadav <yahina8422@gmail.com>
2026-01-28 00:07:52 +0530

    Initial Commit

---
+++ src/main/java/com/config/main/ConfigFilesApplication.java
@@ -0,0 +1,13 @@
+package com.config.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigFilesApplication.class, args);
+	}
+
+}
